6b3132b8659a0d7fb83650d720461e61d997c448
h-hayashi <h-hayashi@froide-kk.co.jp>
2015-03-26 09:53:44 +0900

    初回コミット

---
+++ Nyu-Sya/Controllers/UserController.cs
@@ -0,0 +1,36 @@
+﻿using Nyu_Sya.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Nyu_Sya.Controllers
+{
+    [Authorize]
+    public class UserController : Controller
+    {
+        // GET: Enquete
+        [HttpGet]
+        public ActionResult Enquete()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Enquete(EnqueteViewModel model)
+        {
+            return View("EnqueteResult", model);
+        }
+
+        public ActionResult Download()
+        {
+            return View();
+        }
+
+        public ActionResult Portal()
+        {
+            return View();
+        }
+    }
+}
---
+++ Nyu-Sya/Models/EnqueteViewModels.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Nyu_Sya.Models
+{
+    public class EnqueteViewModel
+    {
+
+        [Display(Name = "扶養家族の有無")]
+        public bool fuyou { get; set; }
+
+        [Display(Name="今年度給与所得の有無")]
+        public bool syotoku { get; set; }
+
+        [Display(Name = "住民税納付状況")]
+        public int juminzei { get; set; }
+
+        [Display(Name="雇用保険被保険者証の有無")]
+        public bool koyouhoken { get; set; }
+    }
+
+    public class DocumentsViewModel
+    {
+        [Display(Name = "基礎年金番号(コピー)")]
+        public bool kisonenkin { get; set; }
+
+        [Display(Name = "給与振込先口座(コピー)")]
+        public bool kyuyokoza { get; set; }
+
+        [Display(Name = "機密保持誓約書(直筆受領)")]
+        public bool kimitsuhoji { get; set; }
+
+        [Display(Name = "誓約書(直筆受領)")]
+        public bool seiyakusyo { get; set; }
+    }
+}
